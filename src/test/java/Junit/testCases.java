979d9bd63df8626f3ff8e3ca79542d179f0a12a5
lgarcia <lleirgarcia@gmail.com>
2017-05-18 21:37:23 +0200

    testcases

---
+++ src/test/java/Junit/testCases.java
@@ -0,0 +1,28 @@
+package Junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by lleir on 18/5/17.
+ */
+
+@RunWith(JUnit4.class)
+public class testCases {
+
+    @Test public void testCase1() throws Exception {
+
+    }
+
+
+    @Test public void testCase2() throws Exception {
+
+    }
+
+
+    @Test public void testCase3() throws Exception {
+
+    }
+
+}
